b3c177d02af0aa688d3cbaea289c877225c185bc
Todor Toshev <t.toshev95@gmail.com>
2021-10-20 14:03:17 +0300

    Registered Categ. and Prod. in dj-admin; defined shop.product_list() view for browsing available products by category.

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+
+    '''
+    list_editable attribute sets the fields that 
+    can be edited from the list display page of the admin site. This allows
+    to edit multiple rows at once. Any field in list_editable must also be listed 
+    in the list_display attribute, since only the fields displayed can be edited.
+    '''
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
