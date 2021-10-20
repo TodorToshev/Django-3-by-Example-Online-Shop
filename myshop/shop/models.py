ba6066686138f75bd5b6243bd53502764ad02ecd
Todor Toshev <t.toshev95@gmail.com>
2021-10-20 15:59:28 +0300

    Added shop.product_detail() view and urlpatterns; get_absolute_url to Categ. & Prod. models; added html templates + css.

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -10,6 +11,9 @@ class Category(models.Model):
         ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -30,5 +34,9 @@ class Product(models.Model):
         ordering = ('name',)
         index_together = (('id', 'slug'),)
         
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
+
     def __str__(self):
         return self.name
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -8,8 +8,15 @@ def product_list(request, category_slug=None):
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
 
+    #if slug is provided, we are rendered a product list, filtered by the slug.
+    #if slug is none, we go to the "" urplattern w/ all products.
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     
     return render(request, 'shop/product/list.html', {'category': category, 'products': products, 'categories': categories})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True) #slug only needed for SEO-friendly URL;
+    return render(request, 'shop/product/detail.html', {'product': product})
