7ded5e8e38126180aaaa31626a3ff3f15e074006
Todor Toshev <t.toshev95@gmail.com>
2021-10-31 14:02:43 +0200

    Added celery tasks.py file in 'orders' app, added necessary settings and imports only to find out about a deprecated module. Async tasks are not going to be included in this project.

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -139,3 +139,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ myshop/orders/tasks.py
@@ -0,0 +1,16 @@
+# from celery import task 
+#apparently 'task' module is deprecated and has been removed in celery 5.
+from django.core.mail import send_mail
+from .models import Order
+
+# @task
+def order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f'Order nr. {order.id}'
+
+    message = f'Dear {order.first_name},\n\n' \
+    f'You have successfully placed an order.' \
+    f'Your order ID is {order.id}.'
+
+    mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
+    return mail_sent
