c37816e5910b1b23287289d9ec4c32891516570e
Todor Toshev <t.toshev95@gmail.com>
2021-10-31 16:29:21 +0200

    Added payment_done and payment_canceled views, html templates and html+js for rendering of the Braintree hosted fields and handling the sent data.

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -8,7 +8,10 @@ from orders.models import Order
 gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
 
 def payment_process(request):
+
+    #the following session key is set by orders.views.order_create()
     order_id = request.session.get('order_id')
+
     order = get_object_or_404(Order, id=order_id)
     total_cost = order.get_total_cost()
 
@@ -33,3 +36,13 @@ def payment_process(request):
         # generate token
         client_token = gateway.client_token.generate()
         return render(request, 'payment/process.html', {'order': order, 'client_token': client_token})
+
+
+def payment_done(request):
+    return render(request, 'payment/done.html')
+
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
+
+
