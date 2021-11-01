454cdbe193beeffb832127b443ef9e5f074e6d6f
Todor Toshev <t.toshev95@gmail.com>
2021-11-01 22:10:35 +0200

    Added custom admin view by adding orders.views.admin_order_detail(), urlpattern for the new view, html template, orders.admin.order_detail, which returns link for the admin_order_detail, and order_detail in OrderAdmin's list_display.

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -3,6 +3,8 @@ from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
@@ -36,9 +38,15 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = ['id', 'first_name', 'last_name', 'email', 
+        'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -4,6 +4,9 @@ from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
 
 def order_create(request):
     cart = Cart(request)
@@ -26,3 +29,9 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
