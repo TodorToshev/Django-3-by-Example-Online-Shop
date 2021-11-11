0b9f12fe8204a2e8ff62c69cca1a337fe22061d1
Todor Toshev <t.toshev95@gmail.com>
2021-11-11 16:57:41 +0200

    Added Quantity for translation in cart.forms

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+<<<<<<< Updated upstream
 from django.utils.translation import gettext_lazy as _
 
+=======
+from django.utils.translation import ugettext_lazy as _
+>>>>>>> Stashed changes
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
